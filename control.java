d898ff7e6fe0e021300886c1e99b11346c63f207
nixxynero <nmdstone@gmail.com>
2017-05-16 13:06:03 -0700

    Sudo code for control
    i added sudo code to work with a basic terminal

--- control.java
+++ control.java
@@ -1,7 +1,53 @@
 import java.io.*;					//import all of java
 
 class control {						//start of the class
+	//set up all global variables
 	public static void main(String[] args) {			//main method that controls everything
-		System.out.println("hey i ran");				//testing println
+		//run init
+		//run toggel 
+	}
+	public static init(){
+		//error checker 
+		System.out.println("inilitilizing");
+		//check if there is a user object or not
+		//yes goto toggle (break?)
+		//no newUser
+	}
+	public static checkDrunk(){
+		//error checker 
+		System.out.println("running check drunk");
+		
+	}
+	public static newUser(){
+		//create new user form teh user model
+		//pring out menu for the user creation form  
+
+		//have option that will allow user to be able to add name 
+		//opt for last name 
+		//option for age 
+		//opt that will check if legal 
+		//apt email 
+		//once data is filled start set up test 
+		//break back to main
+	}
+	public static newTest(){
+		//begin test 
+		//display question 
+		//check if question is correct
+		//set bool corr to true or false 
+		//add to int score 
+		//run score calculate
+		//add intel level to the user 
+		//break
+	}
+	public static scoreCalc(){
+		//find the % score 
+		//write score to variable (double)
+		//check what intel level the user is
+		//write the intel.
+		//break
+	}
+	public static toggel(){
+		//sudo code here lol (simple if u cant figure this out kys)
 	}
 }
